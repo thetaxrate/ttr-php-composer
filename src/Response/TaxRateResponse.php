f64b503def4360f457e8f72fe759b96702acd473
Danny Bolick <danny.bolick@>
2021-12-31 16:15:01 -0500

    Basic usage of the API request

---
+++ src/Response/TaxRateResponse.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace TheTaxRate\SalesTaxProvider\Response;
+
+class TaxRateResponse
+{
+    private $_rate = 0;
+
+   public function __construct($data = null)
+   {
+       if(is_null($data))return;
+
+       $json = json_decode($data,true);
+       if(isset($json['rate']))$this->_rate = $json['rate'];
+   }
+
+   public function getRate()
+   {
+       return $this->_rate;
+   }
+}
--- src/SalesTaxService.php
+++ src/SalesTaxService.php
@@ -2,12 +2,60 @@
 
 namespace TheTaxRate\SalesTaxProvider;
 
+use GuzzleHttp\Client;
+use TheTaxRate\SalesTaxProvider\Response\TaxRateResponse;
+
 class SalesTaxService
 {
+    private $_apiToken;
 
 
-    public static function Tester()
+    private $_baseUrl = 'www.thetaxrate.com/external/v1/tax_rate/%s';
+
+    private $_version = 'v1';
+
+    private $_apiPath = '/external/%s/tax_rate/';
+
+
+    public function __construct($apiToken)
     {
-        return 'TESTER';
+        $this->_apiToken = $apiToken;
+        $this->_apiPath = sprintf($this->_apiPath,$this->_version);
     }
+
+    public function getTaxRate($zipCode)
+    {
+        $response = $this->sendTaxRateRequest($zipCode);
+        if($response->getStatusCode() == 200 ||
+            $response->getStatusCode() == 201)
+        {
+            return new TaxRateResponse($response->getBody()->getContents());
+        }
+        return new TaxRateResponse();
+    }
+
+    private function sendTaxRateRequest($zipCode)
+    {
+        return $this->getHttpClient()->request('GET', $this->_apiPath.$zipCode, [
+            'headers' => $this->getRequestHeaders()
+        ]);
+    }
+
+
+    /**
+     * @return Client
+     */
+    private function getHttpClient()
+    {
+       return new Client(['base_uri' => $this->_baseUrl]);
+    }
+
+    private function getRequestHeaders()
+    {
+        return [
+            'Authorization' => 'Bearer ' . $this->_apiToken,
+            'Accept'        => 'application/json',
+        ];
+    }
+
 }
