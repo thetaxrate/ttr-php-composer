03e90c772812e7e104c3f448dce5a2d9ec35b90f
Danny Bolick <danny.bolick@>
2021-12-31 16:28:55 -0500

    Minor changes to flow as desired for basic use

--- src/SalesTaxService.php
+++ src/SalesTaxService.php
@@ -5,51 +5,67 @@ namespace TheTaxRate\SalesTaxProvider;
 use GuzzleHttp\Client;
 use TheTaxRate\SalesTaxProvider\Response\TaxRateResponse;
 
-class SalesTaxService
+final class SalesTaxService
 {
     private $_apiToken;
 
-
-    private $_baseUrl = 'www.thetaxrate.com/external/v1/tax_rate/%s';
-
     private $_version = 'v1';
 
-    private $_apiPath = '/external/%s/tax_rate/';
+    private $_apiPath = '/api/external/%s/tax_rate/';
+
+    private $_baseUrl = 'https://www.thetaxrate.com';
 
 
+    /**
+     * Token for communication with the API
+     * @param $apiToken
+     */
     public function __construct($apiToken)
     {
         $this->_apiToken = $apiToken;
         $this->_apiPath = sprintf($this->_apiPath,$this->_version);
     }
 
+    /**
+     * Get sale tax response for zip code
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
     public function getTaxRate($zipCode)
     {
         $response = $this->sendTaxRateRequest($zipCode);
-        if($response->getStatusCode() == 200 ||
-            $response->getStatusCode() == 201)
+        if(in_array($response->getStatusCode(),[200,201]))
         {
             return new TaxRateResponse($response->getBody()->getContents());
         }
         return new TaxRateResponse();
     }
 
+    /**
+     * Send Request for rate to the API
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
     private function sendTaxRateRequest($zipCode)
     {
-        return $this->getHttpClient()->request('GET', $this->_apiPath.$zipCode, [
+        return $this->getHttpClient()
+            ->request('GET', $this->_baseUrl.$this->_apiPath.$zipCode, [
             'headers' => $this->getRequestHeaders()
         ]);
     }
 
 
     /**
+     * Create Http client
      * @return Client
      */
     private function getHttpClient()
     {
-       return new Client(['base_uri' => $this->_baseUrl]);
+        return new Client();
     }
 
+    /**
+     * Create header values
+     * @return string[]
+     */
     private function getRequestHeaders()
     {
         return [
